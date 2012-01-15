acbf9e933b4b6ffb53968129e7dc2c399f70a32b
David Cuddeback <david.cuddeback@gmail.com>
2012-01-15 15:45:13 -0800

    automatically loads patches in config/patches for Rails applications

--- lib/monkey_patch/railtie.rb
+++ lib/monkey_patch/railtie.rb
@@ -1,7 +1,11 @@
 module MonkeyPatch
   class Railtie < Rails::Railtie
-    initializer "monkey_patch.initialization" do |app|
+    initializer "monkey_patch.initialization" do
       MonkeyPatch.logger = Rails.logger
+
+      Dir.glob Rails.root.join("config/patches/**/*.rb") do |file|
+        require file
+      end
     end
   end
 end
