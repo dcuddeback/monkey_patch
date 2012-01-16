769802b02be41808a2e79508de44335b0a2ecf7b
David Cuddeback <david.cuddeback@gmail.com>
2012-01-15 16:41:06 -0800

    loads patches from a method that can be trapped by Spork

--- lib/monkey_patch/railtie.rb
+++ lib/monkey_patch/railtie.rb
@@ -1,11 +1,16 @@
 module MonkeyPatch
   class Railtie < Rails::Railtie
+
     initializer "monkey_patch.initialization" do
       MonkeyPatch.logger = Rails.logger
+      load_patches!
+    end
 
+    def load_patches!
       Dir.glob Rails.root.join("config/patches/**/*.rb") do |file|
         require file
       end
     end
+
   end
 end
